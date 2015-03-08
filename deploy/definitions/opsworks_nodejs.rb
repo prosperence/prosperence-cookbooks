1cf8eeef7be2d3453e2bd6165c53190f40bc1166
Scott Rice <scottrice10@gmail.com>
2015-03-08 07:49:50 -0700

    Update opsworks_nodejs.rb

--- deploy/definitions/opsworks_nodejs.rb
+++ deploy/definitions/opsworks_nodejs.rb
@@ -30,7 +30,7 @@ define :opsworks_nodejs do
     variables(
       :deploy => deploy,
       :application_name => application,
-      :monitored_script => "#{deploy[:deploy_to]}/current/server.js"
+      :monitored_script => "/usr/local/bin/npm start"
     )
     notifies :restart, "service[monit]", :immediately
   end
